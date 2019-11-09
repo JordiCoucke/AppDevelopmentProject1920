d4bf5c0d6dce00992dac8bf8dc57d580acd40a69
JordiCoucke <s104623@ap.be>
2019-11-09 14:32:48 +0100

    Setup github/kraken with android sudio

--- app/src/main/java/com/example/apappdevelopmentproject1920/MainActivity.java
+++ app/src/main/java/com/example/apappdevelopmentproject1920/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setImageResource(R.drawable.addplayer);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
