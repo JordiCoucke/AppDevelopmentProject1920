0c65d79b24d706f932572eb44e348859cafc4860
JordiCoucke <s104623@ap.be>
2019-11-23 15:27:15 +0100

    can now go to second view, username can be added

---
+++ app/src/main/java/com/example/apappdevelopmentproject1920/DatabaseConnected.java
@@ -0,0 +1,46 @@
+package com.example.apappdevelopmentproject1920;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class DatabaseConnected extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_connected);
+    }
+
+    public void DBListen()
+    {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final DocumentReference docRef = db.collection("gameSessions").document("session");
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists())
+                {
+
+                }
+                    else
+                    {
+                    //data==null
+                    }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/apappdevelopmentproject1920/MainActivity.java
+++ app/src/main/java/com/example/apappdevelopmentproject1920/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.apappdevelopmentproject1920;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,12 +28,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import io.grpc.Context;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private final Context c = null;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -68,38 +78,92 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void HostSession(View view)
+    public void HostSession(final View view)
     {
+
+        EditText nickNameMessage;
+        nickNameMessage = findViewById(R.id.editText3);
+        String nickName = nickNameMessage.getText().toString();
+
         Random r = new Random();
         int rand = r.nextInt(899) + 1000;
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
         Map<String, Object> session = new HashMap<>();
         session.put("ID", rand);
-
+        session.put("nickName", nickName);
+        String IDSessionName = "gameSessions";
         // Add a new document with a generated ID
-        db.collection("sessionID")
-                .add(session)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        db.collection(IDSessionName).document("session")
+                .set(session)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    public void onSuccess(Void avoid) {
+                        Intent intent = new Intent(view.getContext(), DatabaseConnected.class);
+                        startActivity(intent);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        //incase of failure debugging
                     }
                 });
     }
 
-    public void JoinSession(View view)
+    public void JoinSession(final View view)
     {
-        EditText codeMessage;
-        codeMessage = findViewById(R.id.editText2);
-        String code = codeMessage.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        
+        db.collection("gameSessions")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult())
+                            {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                String[] IDArray = new String[document.getData().size()];
+
+                                EditText codeMessage;
+                                codeMessage = findViewById(R.id.editText2);
+                                String sessionCode = codeMessage.getText().toString();
+
+                                int i = 0;
+                                for (Object o: document.getData().values())
+                                {
+                                    IDArray[i] = o.toString();
+                                    if(sessionCode.equals(IDArray[i]))
+                                    {
+                                        Intent intent = new Intent(view.getContext(), DatabaseConnected.class);
+                                        startActivity(intent);
+                                    }
+                                    i++;
+                                }
+
+                                EditText nickNameMessage;
+                                nickNameMessage = findViewById(R.id.editText3);
+                                String nickName = nickNameMessage.getText().toString();
+
+                                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                DocumentReference addNickName = db.collection("gameSessions").document("session");
+                                addNickName
+                                        .update("nickName",nickName );
+
+                            }
+                        }
+                        else
+                            {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+
+                });
+
+
 
     }
+
 }
